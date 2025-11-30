247ba954b18838b789d399c0dde24fed33083817
kaikiat <pohkaikiat98@gmail.com>
2025-11-30 18:53:36 +0800

    chore: update

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 import markdown
 
-URL = "http://localhost:1337/api/articles"
+# URL = "http://localhost:1337/api/articles"
+response = requests.get("https://bright-canvas-817036fc97.strapiapp.com/api/articles")
+print(response.json())
 
 params = {
     "filters[slug][$eq]": "what-s-inside-a-black-hole",
@@ -9,6 +11,8 @@ params = {
 }
 
 response = requests.get(URL, params=params)
+print(response)
+print(response.json())
 article = response.json()["data"][0]
 
 print(article)
